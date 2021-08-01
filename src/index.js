ee925fd707528166af2854b745f560a73aa0fe31
rcalderking <rcalderking@gmail.com>
2021-08-01 16:08:17 -0500

    sure
    suuure

--- src/index.js
+++ src/index.js
@@ -15,16 +15,16 @@ const createWindow = () => {
       devTools: false
     }
   });
-
+  mainWindow.maximize();
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.maximize();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
+mainWindow.maximize();
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
